aefaa26487b2b946df5dd7fbed0d3f0f23377747
Sena <57217392+SENA-PlumLab@users.noreply.github.com>
2021-08-02 23:57:25 +0900

    Delete teamP_06/java/teamP_06 directory

--- teamP_06/java/teamP_06/Model.java
+++
@@ -1,9 +0,0 @@
-package teamP_06;
-
-public class Model {
-	public void addAttribute(String key, Object ob) {
-		//System.out.println("*** ( 모델 데이터 처리 )");
-		//System.out.println("*** (key:"+key+", 객체: "+ob.getClass().getSimpleName()+")");
-	}
-}
-
