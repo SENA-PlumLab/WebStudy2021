07c1452a3c2c15f899fc9a2be9211e96eb0e7554
Senna-Lab <8916317@naver.com>
2021-12-23 23:08:23 +0900

    created AppRouter

--- src/ApiService.js
+++ src/ApiService.js
@@ -1,7 +1,7 @@
-import { API_BASE_URL } from "./api-config";
+import { API_BASE_URL } from "./app-config";
 
 export function call(api, method, request){
-    const options = {
+    let options = {
         headers: new Headers({
             "Content-Type":"application/json",
         }),
@@ -14,15 +14,36 @@ export function call(api, method, request){
         options.body = JSON.stringify(request);
     }
     
-    return fetch(options.url, options).then( (response) => 
-        response.json().then( (json) => {
-            if(!response.ok) {
-                //response.ok가 true 이면 정상적인 응답을 받은 것이고,...
-                //true가 아니면 에러 응답임
-                return Promise.reject(json);
-            }
-            return json;
-
-        })
-    );
-}
+    return fetch(options.url, options)
+            .then( (response) => 
+                 {
+                     console.log(response);
+                     if(response.status === 403) {
+                        window.location.href = "/login"; //redirect
+                     }
+                     else if(!response.ok) {
+                        
+                        return Promise.reject(response);
+                    }
+                    return response;
+                     /*
+                    if(!response.ok) {
+                        //response.ok가 true 이면 정상적인 응답을 받은 것이고,...
+                        //true가 아니면 에러 응답임
+                        return Promise.reject(json);
+                    }
+                    return json;
+                    */
+                }
+            )
+            .catch((error) => {
+                //접근 거부 라우팅 추가.. 아니 안먹어서 ㅠㅠㅠ 위 코드를 수정함.
+                console.log('error: ', error);
+                console.log('status: ', error.status);
+                console.log('message: ', error.message)
+                if(error.status === 403){
+                    window.location.href = "/login"; //redirect
+                }
+                return Promise.reject(error);
+            });
+};
---
+++ src/AppRouter.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./index.css";
+import App from "./App";
+import Login from "./Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Box,Typography } from "@material-ui/core";
+
+function Copyright(){
+    return (
+        <Typography variant="body2" color="textSecondary" algin="center">
+            {"Copyright © "}
+            Senna-Lab, {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    );
+}
+
+class AppRouter extends React.Component {
+    render() {
+        return(
+            <div>
+                <Router>
+                    <div>
+                        <Routes>
+                            <Route path="/login" element={<Login/>} />
+                            <Route path="/" element={<App />} />
+                        </Routes>
+                    </div>
+                    <Box mt={5}>
+                        <Copyright />
+                    </Box>
+                </Router>
+            </div>
+        );
+    }
+}
+
+export default AppRouter;
