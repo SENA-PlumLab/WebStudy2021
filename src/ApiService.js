4859137d8e630d026f01f3bb23a11bb565eb9831
Senna-Lab <8916317@naver.com>
2021-12-25 23:23:15 +0900

    edit Login service

--- src/ApiService.js
+++ src/ApiService.js
@@ -1,10 +1,22 @@
 import { API_BASE_URL } from "./app-config";
 
+const ACCESS_TOKEN = "ACCESS_TOKEN";
+
 export function call(api, method, request){
+
+    let headers = new Headers({
+        "Content-Type":"application/json",
+        'Accept': 'application/json',
+    });
+
+    // localStrorage에서 ACCESS_TOKEN 가져오기
+    const accessToken = localStorage.getItem(ACCESS_TOKEN);
+    if(accessToken && accessToken!==null){
+        headers.append("Authorization", "Bearer "+accessToken);
+    }
+
     let options = {
-        headers: new Headers({
-            "Content-Type":"application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
@@ -13,37 +25,53 @@ export function call(api, method, request){
         //GET method
         options.body = JSON.stringify(request);
     }
-    
+
     return fetch(options.url, options)
-            .then( (response) => 
-                 {
-                     console.log(response);
-                     if(response.status === 403) {
-                        window.location.href = "/login"; //redirect
-                     }
-                     else if(!response.ok) {
-                        
-                        return Promise.reject(response);
-                    }
-                    return response;
-                     /*
-                    if(!response.ok) {
-                        //response.ok가 true 이면 정상적인 응답을 받은 것이고,...
-                        //true가 아니면 에러 응답임
+            .then( (response) => {
+
+                response.json().then((json) => {
+
+                    if(!response.ok){
                         return Promise.reject(json);
                     }
                     return json;
-                    */
-                }
-            )
+                })
+            })
             .catch((error) => {
-                //접근 거부 라우팅 추가.. 아니 안먹어서 ㅠㅠㅠ 위 코드를 수정함.
                 console.log('error: ', error);
-                console.log('status: ', error.status);
-                console.log('message: ', error.message)
                 if(error.status === 403){
                     window.location.href = "/login"; //redirect
                 }
                 return Promise.reject(error);
             });
-};
+}
+
+export function signin(userDTO) {
+    return call("/auth/signin", "POST", userDTO)
+            .then( (response) => {
+                console.log("response: ", response);
+
+                if(response.token){
+                    localStorage.setItem(ACCESS_TOKEN, response.token);
+                    window.location.href="/";
+                }
+            });
+}
+/*
+export function signin(userDTO) {
+    return call("/auth/signin", "POST", userDTO)
+            .then( (response) => {
+                response.json().then(json => {
+                    //console.log("json: ", json);
+                    //alert("로그인 토큰: "+json.token);
+
+                    if(json.token) {
+                        //token이 존재하는 경우!
+
+                        localStorage.setItem("ACCESS_TOKEN", json.token);
+                        window.location.href="/";
+                    }
+                })
+            });
+}
+*/
